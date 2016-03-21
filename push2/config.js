417ffc12baed036ae98ab778199d041818de4631
yzuykov <yzuykov@yandex.ru>
2016-03-21 22:32:49 +0300

    Add Push2

---
+++ push2/config.js
@@ -0,0 +1,4 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+  gcmAPIKey: 'AIzaSyBvtaUg-pGp-EC7E_O4HhX6ZBA-dAgh_-0'
+};
---
+++ push2/service-worker.js
@@ -0,0 +1,96 @@
+'use strict';
+
+var API_ENDPOINT = 'https://android.googleapis.com/gcm/send';
+
+function showNotification(title, body, icon, data) {
+  var notificationOptions = {
+    body: body,
+    icon: icon ? icon : 'images/touch/chrome-touch-icon-192x192.png',
+    tag: 'simple-push-demo-notification',
+    data: data
+  };
+
+  self.registration.showNotification(title, notificationOptions);
+  return;
+}
+
+self.addEventListener('push', function(event) {
+  console.log('Received a push message', event);
+
+  // Since this is no payload data with the first version
+  // of Push notifications, here we'll grab some data from
+  // an API and use it to populate a notification
+  event.waitUntil(
+    fetch(API_ENDPOINT).then(function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        // Throw an error so the promise is rejected and catch() is executed
+        throw new Error();
+      }
+
+      // Examine the text in the response
+      return response.json().then(function(data) {
+        var title = 'You have a new message';
+        var message = data.message;
+        var icon = 'images/notification-icon.png';
+        var notificationTag = 'chat-message';
+
+        var notificationFilter = {
+          tag: notificationTag
+        };
+        return self.registration.getNotifications(notificationFilter)
+          .then(function(notifications) {
+            if (notifications && notifications.length > 0) {
+              // Start with one to account for the new notification
+              // we are adding
+              var notificationCount = 1;
+              for (var i = 0; i < notifications.length; i++) {
+                var existingNotification = notifications[i];
+                if (existingNotification.data &&
+                  existingNotification.data.notificationCount) {
+                  notificationCount +=
+existingNotification.data.notificationCount;
+                } else {
+                  notificationCount++;
+                }
+                existingNotification.close();
+              }
+              message = 'You have ' + notificationCount +
+                ' weather updates.';
+              notificationData.notificationCount = notificationCount;
+            }
+
+            return showNotification(title, message, icon, notificationData);
+          });
+      });
+    }).catch(function(err) {
+      console.error('Unable to retrieve data', err);
+
+      var title = 'An error occured';
+      var message = 'We were unable to get the information for this ' +
+        'push message';
+
+      return showNotification(title, message);
+    })
+  );
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('On notification click: ', event);
+
+  if (Notification.prototype.hasOwnProperty('data')) {
+    console.log('Using Data');
+    var url = event.notification.data.url;
+    event.waitUntil(clients.openWindow(url));
+  } else {
+    event.waitUntil(getIdb().get(KEY_VALUE_STORE_NAME,
+event.notification.tag).then(function(url) {
+      // At the moment you cannot open third party URL's, a simple trick
+      // is to redirect to the desired URL from a URL on your domain
+      var redirectUrl = '/redirect.html?redirect=' +
+        url;
+      return clients.openWindow(redirectUrl);
+    }));
+  }
+});
